a9bf2e867f7c8e9044bf00e4c3dc577cc50d268f
htmltest <struev.vs@gmail.com>
2017-01-11 15:15:42 +0300

    update+mobile

--- js/tools.js
+++ js/tools.js
@@ -14,16 +14,26 @@ $(document).ready(function() {
     });
 
     $('.gallery-item a, .main-gallery-item a').fancybox({
+        prevEffect: 'none',
+        nextEffect: 'none',
+        margin: 0,
+        padding: 0,
+        maxWidth: 970,
+        minWidth: 480,
+        topRatio: 0,
+        aspectRatio: true,
         tpl : {
             closeBtn : '<a title="Закрыть" class="fancybox-item fancybox-close" href="javascript:;"></a>',
             next     : '<a title="Следующая" class="fancybox-nav fancybox-next" href="javascript:;"><span></span></a>',
             prev     : '<a title="Предыдущая" class="fancybox-nav fancybox-prev" href="javascript:;"><span></span></a>'
         },
         helpers: {
-            overlay : {
-                locked : false
-            }
-        }
+			thumbs	: {
+				width	: 86,
+				height	: 58
+			}
+        },
+        beforeShow: function() { this.title += '<div class="fancybox-title-date">' + $(this.element).data('date') + '</div><a href="' + $(this.element).attr('href') + '" download class="fancybox-download-link"></a>'}
     });
 
     $.validator.addMethod('maskPhone',
@@ -59,8 +69,69 @@ $(document).ready(function() {
 
             $('.geography-list.active').removeClass('active');
             $('.geography-list').eq(curIndex).addClass('active');
+            $('.geography-menu-value').html($(this).html());
+            $('.geography-menu-wrap').removeClass('open');
         }
         e.preventDefault();
+    });
+
+    $('.geography-menu-value').click(function(e) {
+        $(this).parent().toggleClass('open');
+        e.preventDefault();
+    });
+
+    $(document).click(function(e) {
+        if ($(e.target).parent().filter('.geography-menu-wrap').length == 0) {
+            $('.geography-menu-wrap').removeClass('open');
+        }
+    });
+
+    $('.geography-item-feedback-link').click(function(e) {
+        var curBlock = $(this).parent();
+        if (curBlock.hasClass('open')) {
+            curBlock.removeClass('open');
+        } else {
+            $('.geography-item-feedback.open').removeClass('open');
+            curBlock.addClass('open');
+        }
+        e.preventDefault();
+    });
+
+    $(document).click(function(e) {
+        if ($(e.target).parents().filter('.geography-item-feedback').length == 0) {
+            $('.geography-item-feedback.open').removeClass('open');
+        }
+    });
+
+    $('.footer-feedback-link').click(function(e) {
+        var curBlock = $(this).parent();
+        curBlock.toggleClass('open');
+        e.preventDefault();
+    });
+
+    $(document).click(function(e) {
+        if ($(e.target).parents().filter('.footer-feedback').length == 0) {
+            $('.footer-feedback.open').removeClass('open');
+        }
+    });
+
+    $('.nav-mobile-link').click(function(e) {
+        $('html').toggleClass('nav-mobile-open');
+        e.preventDefault();
+    });
+
+    $('.nav-mobile > ul > li.with-submenu > a').click(function(e) {
+        $(this).parent().toggleClass('open');
+        e.preventDefault();
+    });
+
+    $('.services-menu-mobile').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        adaptiveHeight: true,
+        arrows: false,
+        dots: true
     });
 
 });
@@ -81,12 +152,48 @@ $(window).on('load resize', function() {
             } else {
                 curPoint.removeClass('right');
             }
+            if (curWidth < 1200) {
+                if (curWidth - (curLeft * curScale) < 215) {
+                    curPoint.find('.main-scheme-point-content').css({'margin-left': (-215 - (215 - (curWidth- (curLeft * curScale)))) + 'px'});
+                }
+                if (curLeft * curScale < 215) {
+                    curPoint.find('.main-scheme-point-content').css({'margin-left': (-215 + (215 - curLeft * curScale)) + 'px'});
+                }
+            } else {
+                curPoint.find('.main-scheme-point-content').removeAttr('style');
+            }
         });
     });
 });
 
+$(window).on('load resize', function() {
+    if ($(window).width() < 1200) {
+        if (!$('.main-gallery-list').hasClass('slick-slider')) {
+            $('.main-gallery-list').slick({
+                infinite: true,
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                adaptiveHeight: true,
+                arrows: false,
+                dots: true
+            });
+        }
+    } else {
+        if ($('.main-gallery-list').hasClass('slick-slider')) {
+            $('.main-gallery-list').slick('unslick');
+        }
+    }
+});
+
+$(window).on('resize', function() {
+    $('.form-select select').chosen('destroy');
+    $('.form-select select').chosen({disable_search: true, placeholder_text_multiple: ' ', no_results_text: 'Нет результатов'});
+});
+
 function initForm(curForm) {
     curForm.find('input.maskPhone').mask('+7 (999) 999-99-99');
+
+    curForm.find('.form-select select').chosen({disable_search: true, no_results_text: 'Нет результатов'});
 
     curForm.find('.form-file input').change(function() {
         var curInput = $(this);
@@ -106,12 +213,17 @@ function initForm(curForm) {
 }
 
 function checkErrors() {
-    $('.form-file').each(function() {
+    $('.form-checkbox, .form-input, .form-file').each(function() {
         var curField = $(this);
-        if (curField.find('input.error').length > 0) {
+        if (curField.find('input.error').length > 0 || curField.find('textarea.error').length > 0) {
             curField.addClass('error');
         } else {
             curField.removeClass('error');
+        }
+        if (curField.find('input.valid').length > 0 || curField.find('textarea.valid').length > 0) {
+            curField.addClass('valid');
+        } else {
+            curField.removeClass('valid');
         }
     });
 }
